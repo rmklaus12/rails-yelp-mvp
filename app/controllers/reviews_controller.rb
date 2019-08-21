2322a5c68ef7578457a7385ded2634aca0183f59
Robin Klaus <rmklaus12@gmail.com>
2019-08-21 15:51:39 +1000

    rake passing all tests. Need to do front end work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,16 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-  def create
-    @review = Review.new(review_params)
+  def show
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review.restaurant = @restaurant
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id]) # find restaurant to assign review to
+    @review = Review.new(review_params)                   # create new review
+    @review.restaurant = @restaurant                      # link new review to restaurant
+
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
@@ -18,6 +24,14 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
+
+  # def create
+  #   @review = Review.new(review_params)
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review.restaurant = @restaurant
+  #   @review.save
+  #   redirect_to restaurant_path(@restaurant)
+  # end
